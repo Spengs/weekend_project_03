00640bfd92659231324e86964f75122f271df635
Spengs <Jotto465@gmail.com>
2016-08-13 19:41:13 -0500

    index done, server & client started

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  console.log("working");
+  $('#songForm').on('submit', function(){
+    event.preventDefault();
+
+    var calcVals = {};
+
+    $.each($('#calcForm').serializeArray(), function(i, field){
+      calcVals[field.name] = field.value;
+    });
 })
